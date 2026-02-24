3c79d7d0e0134f5d3e91ca363f9e863e845cff76
owen <22165146+sudo-owen@users.noreply.github.com>
2026-02-24 13:57:13 -0800

    wip

--- script/Surgery.s.sol
+++ script/Surgery.s.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 import "forge-std/Script.sol";
 
 import {IEngine} from "../src/IEngine.sol";
-import {SignedCommitManager} from "../src/commit-manager/SignedCommitManager.sol";
+import {BetterCPU} from "../src/cpu/BetterCPU.sol";
+import {ICPURNG} from "../src/rng/ICPURNG.sol";
+import {ITypeCalculator} from "../src/types/ITypeCalculator.sol";
 
 struct DeployData {
     string name;
@@ -17,8 +19,8 @@ contract Surgery is Script {
     function run() external returns (DeployData[] memory) {
         vm.startBroadcast();
 
-        SignedCommitManager m = new SignedCommitManager(IEngine(0x4F198ba502572c3C8d43c246E610d2B64b089fA1));
-        deployedContracts.push(DeployData({name: "SIGNED COMMIT MANAGER", contractAddress: address(m)}));
+        BetterCPU cpu = new BetterCPU(4, IEngine(0xaE14d4eFD7F30AFA679CD7e971f7b6CE9C445329), ICPURNG(address(0)), ITypeCalculator(0x65BcF9e5a0A6adedB6BA71b86eb255E9e9aF65dF));
+        deployedContracts.push(DeployData({name: "Better CPU", contractAddress: address(cpu)}));
 
         vm.stopBroadcast();
         return deployedContracts;
